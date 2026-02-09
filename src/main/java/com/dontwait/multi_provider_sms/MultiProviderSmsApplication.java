223bc468b5872a556513f63718b68e8194cf685b
dont-wait <nguyentansang15042005@gmail.com>
2026-02-10 04:24:39 +0700

    load environment variables using dotenv

--- src/main/java/com/dontwait/multi_provider_sms/MultiProviderSmsApplication.java
+++ src/main/java/com/dontwait/multi_provider_sms/MultiProviderSmsApplication.java
@@ -3,10 +3,16 @@ package com.dontwait.multi_provider_sms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class MultiProviderSmsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+	    dotenv.entries().forEach(entry -> 
+	        System.setProperty(entry.getKey(), entry.getValue())	   
+		);	
 		SpringApplication.run(MultiProviderSmsApplication.class, args);
 	}
 
